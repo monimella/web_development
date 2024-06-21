aae97a86a18d5127a9cb805c44617c4c9e703ad1
monimella <monimella@gmail.com>
2024-06-20 20:53:48 -0400

    Add files via upload

--- lab12_events/script.js
+++ lab12_events/script.js
@@ -105,3 +105,74 @@ window.addEventListener("scroll", function(){
     gotop.style.display="none"
 }
 })
+
+/*Thursday, June 20---*/
+/*FORM EVENTS*/
+/*INPUT EVENT*/
+//get reference to the form elements
+
+const myform = document.querySelector("#myform")
+
+//get the <div class="greeting">
+const greeting = document.querySelector(".greeting")
+const greetingname = document.querySelector(".greeting p span")
+
+myform.addEventListener("submit", function(event){
+
+
+    //prevent the default form submission behavior
+    event.preventDefault()
+
+    //start form validation, username
+    const usernameinput=document.querySelector("#username")
+    //collect the input text value
+    const username = usernameinput.value
+    
+
+    //validation1: make sure the user types a username before pressing the submit button
+ if (username.trim() ---""){
+    return; //stop further execution
+}
+    //If the validation passed, you can submit the data to the server
+    //greeting message after the validation
+    greetingname.innerHTML = usernamegreeting.style.display="block"
+
+    //after the form is submitted, we can clear the username
+    usernameinput.value=""
+
+
+/**
+ * password validation
+ */
+  //collect form elements
+ 
+ const passwordfield=document.querySelector("#passwordfield")
+ const submitbtn=document.querySelector("submitbtn")
+ //collect the password error message element
+ const passworderror=document.querySelector("passworderror")
+
+ //disabled button when load the window
+ window.addEventListener("load",function(){
+    submitbtn.disabled=true
+    submitbtn.style.backgroundColor="lightgray"
+ })
+
+    //check the length of the password
+    passwordfield.addEventListener ("input",function(){
+
+    
+    let numbercharacter=password.field.value.length
+    if(numbercharacters<8){
+        passworderror.textContent ="Password must be 8+ characters..."
+        passworderror.style.color="red";
+    }
+    else{
+        passworderror.innerHTML="&#x2713;"
+        passworderror.style.color="green"
+        passwordfield.style.border="solid 2px green"
+        submitbtn.disabled =false
+        submitbtn.style.backgroundColor="red"
+    }
+
+    
+ })
